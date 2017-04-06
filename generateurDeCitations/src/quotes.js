905b8eeb18be3f0fadc3d1f6d7abe5a9646fb639
rlecostey <remy.lecostey@gmail.com>
2017-04-06 18:59:48 +0200

    add react exercise quoteGenerator

---
+++ generateurDeCitations/src/quotes.js
@@ -0,0 +1,42 @@
+module.exports = {
+  quote1: {
+    actor: 'vandam',
+    quote: 'Moi, Adam et Eve, j’y crois plus tu vois, parce que je suis pas un idiot : la pomme ça peut pas être mauvais, c’est plein de pectine…',
+   },
+  quote2: {
+    actor: 'vandam',
+    quote: 'Selon les statistiques, il y a une personne sur cinq qui est déséquilibrée. S\'il y a 4 personnes autour de toi et qu\'elles te semblent normales, c\'est pas bon.',
+  },
+  quote3: {
+    actor: 'vandam',
+    quote: 'Une vache, ça te bouffe trois hectares, moi, avec trois hectares, je te fais deux mille kilos de riz... avec trois hectares, je te nourris Avignon, tu vois...',
+  },
+  quote4: {
+    actor: 'vandam',
+    quote: 'Quand tu prends confiance en la confiance tu deviens confiant.',
+  },
+  quote5: {
+    actor: 'vandam',
+    quote: 'La drogue, c\'est comme quand tu close your eyes et que tu traverses la rue.',
+  },
+  quote6: {
+    actor: 'chuck-norris',
+    quote: 'Google, c\'est le seul endroit où tu peux taper Chuck Norris...',
+  },
+  quote7: {
+    actor: 'chuck-norris',
+    quote: 'Pour certains hommes le testicule gauche est plus large que le testicule droit, chez Chuck Norris, chaque testicule est plus large que l’autre.',
+  },
+  quote8: {
+    actor: 'chuck-norris',
+    quote: 'Les suisses ne sont pas neutres, ils attendent de savoir de quel coté Chuck Norris se situe.',
+  },
+  quote9: {
+    actor: 'chuck-norris',
+    quote: 'Si Chuck Norris attaque l’empire, l’empire ne contre attaque pas.',
+  },
+  quote10: {
+    actor: 'chuck-norris',
+    quote: 'Chuck Norris n’utilise pas de suppositoire. Les voies du seigneur sont impénétrables.',
+  }
+};
